d50325efd942cf6ede3123b684f4e00db4ad5290
Waldemar Schröer <w@ldemar.de>
2026-01-22 08:57:36 +0100

    Minor changes

--- TX_Arduino_Nano/RZ-RC-v1.transmitter.ino
+++ TX_Arduino_Nano/RZ-RC-v1.transmitter.ino
@@ -15,25 +15,54 @@
   |                  waldemar.schroeer(at)rz-amper.de                          |
   +----------------------------------------------------------------------------+*/
 
-boolean debug = false;                                                          // Debugging on/off
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128                                                        // OLED display width, in pixels
-#define SCREEN_HEIGHT 64                                                        // OLED display height, in pixels
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL),
-// On an arduino Nano:      A4(SDA), A5(SCL),
-#define OLED_RESET     -1                                                       // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C                                                     // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-#define DELAY_TIME 5000                                                         // Delay time between measurements
-#define UpdateScreenInterval 500                                                // Update screen interval in milliseconds
+// --------------------- Settings ----------------------------------------------
+        // Arduino Nano
+        // Define input pins of the rotary encoder
+        // If you use a Rotary Encoder breakout board, leave pin V+ unconnected
+        #define SCREEN_WIDTH 128                                                // OLED display width, in pixels
+        #define SCREEN_HEIGHT 64                                                // OLED display height, in pixels
+        // Define input pins of the rotary encoder
+        // If you use a Rotary Encoder breakout board, leave pin V+ unconnected
+        #define CLK_PIN 3                                                       // pin D3 connected to rotary encoder CLK (OUT A)
+        #define DT_PIN 4                                                        // pin D4 connected to rotary encoder DT (OUT B)
+        #define SW_PIN 2                                                        // pin D2 connected to rotary encoder SW (SWITCH)
+
+        // oLED Display settings
+        #define OLED_RESET -1                                                   // Reset pin # (or -1 if sharing Arduino reset pin)
+        #define SCREEN_ADDRESS 0x3C                                             // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+        #define SCREEN_WIDTH 128                                                // OLED display width, in pixels
+        #define SCREEN_HEIGHT 64                                                // OLED display height, in pixels
+        // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+        // The pins for I2C are defined by the Wire-library.
+        // On an arduino UNO:       A4(SDA), A5(SCL)
+        // On an arduino MEGA 2560: 20(SDA), 21(SCL)
+        // On an arduino LEONARDO:   2(SDA),  3(SCL),
+        // On an arduino Nano:      A4(SDA), A5(SCL),
+
+        // Misc settings
+        boolean debug = false;                                                  // Debugging on/off
+        boolean serialDebug = false;                                            // Serial debugging on/off
+        const int iLED= 2;                                                      // onboard indicator led gpio pin
+        const int menuTimeout = 10;                                             // menu inactivity timeout (seconds)
+        const bool menuLargeText = 0;                                           // show larger text when possible (if struggling to read the small text)
+        const int maxmenuItems = 12;                                            // max number of items used in any of the menus (keep as low as possible to save memory)
+        const int itemTrigger = 2;                                              // rotary encoder - counts per tick (varies between encoders usually 1 or 2)
+        const int topLine = 18;                                                 // y position of lower area of the display (18 with two colour displays)
+        const byte lineSpace1 = 9;                                              // line spacing for textsize 1 (small text)
+        const byte lineSpace2 = 17;                                             // line spacing for textsize 2 (large text)
+        const int displayMaxLines = 5;                                          // max lines that can be displayed in lower section of display in textsize1 (5 on larger oLeds)
+        const int MaxmenuTitleLength = 10;                                      // max characters per line when using text size 2 (usually 10)
+        #define IntervalScreenUpdate 250                                        // Screen update interval in milliseconds
+        #define IntervalSensorRead 10                                           // Sensor read interval in milliseconds
+        #define IntervalRotaryRead 10                                           // Rotary encoder read interval in milliseconds
+        #define IntervalDebug 1000                                              // Debug output interval in milliseconds
+// -----------------------------------------------------------------------------
 
 // Macro for time intervals
 #define every(interval)\
@@ -42,11 +71,6 @@ boolean debug = false;                                                          
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);       // Create display object
 
-// Define input pins of the rotary encoder
-// If you use a Rotary Encoder breakout board, leave pin V+ unconnected
-#define CLK_PIN 3                                                              // pin D3 connected to rotary encoder CLK (OUT A)
-#define DT_PIN 4                                                               // pin D4 connected to rotary encoder DT (OUT B)
-#define SW_PIN 2                                                               // pin D2 connected to rotary encoder SW (SWITCH)
 
 
 
@@ -122,7 +146,33 @@ void readAnalogValues() {
 // Function to read the rotary encoder
 void readRotaryEncoder() {
   // refactor as function now
+  rotaryCLKstate = digitalRead(CLK_PIN);                                        // Read the current state of CLK
+  if (rotaryCLKstate != rotaryPrevCLKstate && rotaryCLKstate == HIGH) {         // Has the rotary encoder moved?
+    if (digitalRead(DT_PIN) == HIGH) {
+      rotaryCounter++;                                                          // Counter-Clockwise
+      rotarydirection = DIRECTION_L;
+    } else {
+      rotaryCounter--;                                                          // Clockwise
+      rotarydirection = DIRECTION_R;
+    }
+    Serial.print("Direction:");
+    if (rotarydirection == DIRECTION_R) {
+      Serial.println("R ");
+    } else {
+      Serial.println("L ");
+    }
+  }
+  rotaryPrevCLKstate = rotaryCLKstate;                                          // Store current state as previous state for next loop
+  rotaryButtonState = digitalRead(SW_PIN);                                      // Read the current state of BUTTON
 
+  if (rotaryButtonState != rotaryPrevButtonState) {                             // Has the button state changed?
+    if (rotaryButtonState == LOW) {
+      Serial.println("The button is PRESSED.");
+    } else {
+      Serial.println("The button is RELEASED.");
+    }
+  rotaryPrevButtonState = rotaryButtonState;                                    // Store current state as previous state for next loop
+  }
 }
 
 // --------------------- void setup() ------------------------------------------
@@ -166,45 +216,13 @@ void setup() {
 // --------------------- void loop() -------------------------------------------
 void loop() {
 
-  rotaryCLKstate = digitalRead(CLK_PIN);                                                       // Read the current state of CLK
-  if (rotaryCLKstate != rotaryPrevCLKstate && rotaryCLKstate == HIGH) {
-    if (digitalRead(DT_PIN) == HIGH) {
-      rotaryCounter++;                                                          // CW
-      rotarydirection = DIRECTION_R;
-    } else {
-      rotaryCounter--;                                                          // CCW
-      rotarydirection = DIRECTION_L;
-    }
-    Serial.print("Direction:");
-    if (rotarydirection == DIRECTION_R) {
-      Serial.println("R ");
-    } else {
-      Serial.println("L ");
-    }
-}
-rotaryPrevCLKstate = rotaryCLKstate;                                                           // Store current state as previous state for next loop
-rotaryButtonState = digitalRead(SW_PIN);
-
-  if (rotaryButtonState != rotaryPrevButtonState) {
-    if (rotaryButtonState == LOW) {
-      Serial.println("The button was PRESSED.");
-    } else {
-      Serial.println("The button was RELEASED.");
-    // remember current state for next time:
-    rotaryPrevButtonState = rotaryButtonState; 
-    }
+  every(IntervalSensorRead) {                                                   // Read analog values at defined interval
+    readAnalogValues();
   }
-
-
-  readAnalogValues();
-  if (debug) {                                                                  // Debugging output
-    Serial.print("Ch1:");
-    Serial.println(ValForChannel1);
-    Serial.print("Ch2:");
-    Serial.println(ValForChannel2);
+  every(IntervalRotaryRead)  {                                                  // Read rotary encoder at defined interval
+    readRotaryEncoder();
   }
-
-  every(UpdateScreenInterval) {
+  every(IntervalScreenUpdate) {                                                 // Update screen at defined interval
     drawFrame();
     drawChannelInfo(1, 2, ValForChannel1);
     drawChannelInfo(2, 12, ValForChannel2);
@@ -213,6 +231,16 @@ rotaryButtonState = digitalRead(SW_PIN);
     drawChannelInfo(5, 42, ValForAux1);
     drawChannelInfo(6, 52, ValForAux2);
     display.display();
+  }
+  if (debug) {
+    every(IntervalDebug)  {                                                     // Output debug information at defined interval
+      Serial.print("Rotary:");
+      Serial.println(rotaryCounter);
+      Serial.print("Ch1:");
+      Serial.println(ValForChannel1);
+      Serial.print("Ch2:");
+      Serial.println(ValForChannel2);
+    }
   }
 }
 // --------------------- END ---------------------------------------------------
